bbef5b4140571edf71e307eaf1de66826d3269e2
Tanav Palisetti <56857569+TanavP2112@users.noreply.github.com>
2024-01-13 14:11:47 -0600

    intial progress on website

---
+++ src/components/SpotlightPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./SpotlightPage.css";
+
+function SpotlightPage() {
+  return (
+    <div className='spotlight-container'>
+      <video src='/videos/video-bg.mp4' autoPlay loop muted />
+      <h1>WELCOME</h1>
+      <p>Feel Free to Explore Around!</p>
+      <div className='spotlight-btns'>
+        <Button className='btns' buttonStyle='btn--outline'>
+          See My Projects!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default SpotlightPage;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import "../../App.css";
+import SpotlightPage from "../SpotlightPage";
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <SpotlightPage />
+    </>
+  );
+}
+
+export default Home;
