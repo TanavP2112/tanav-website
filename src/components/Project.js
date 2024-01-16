7b543ddc9230b6a2d7709e2f1cdc904e44625d35
Tanav Palisetti <56857569+TanavP2112@users.noreply.github.com>
2024-01-16 13:54:52 -0600

    edited some text fonts

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import "./Project.css";
 
 function Project() {
   return (
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,8 +6,8 @@ function ProjectItem(props) {
       <li className='project-item'>
         <img src='' alt='' />
         <div className='project-item-info'>
-          <h5 className='card-item-head'>{props.heading}</h5>
-          <p className='card-item-txt'>{props.text}</p>
+          <h5 className='project-item-head'>{props.heading}</h5>
+          <p className='project-item-txt'>{props.text}</p>
         </div>
       </li>
     </>
